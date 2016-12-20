6a76a7eb34326d62df94eede27f16dabe601d9b9
Wojciech Olszowy <wolszowy.contact@gmail.com>
2016-12-20 18:03:49 +0100

    Dodanie JS do sekcji O PROJEKCIE

--- js/app.js
+++ js/app.js
@@ -2,13 +2,9 @@ $(function(){
 
     var nav = $('.stickyNav');
     var logoNav = nav.find('.logoNav');
-    console.log(logoNav);
 
     function scroll(){
          var windowScrollTop = $(window).scrollTop();
-
-        console.log(windowScrollTop);
-        // console.log(menuTop);
 
         if(windowScrollTop > 0){
             nav.addClass('sticky');
@@ -25,35 +21,69 @@ $(function(){
 
     scroll();
 
-  //   function zadanie2(){
-  //       var menu = $('.menu');
-   //
-   //
-  //       var a = menu.find('a');
-   //
-  //       a.on('click', function(){
-  //           var href = $(this).attr('href');
-  //           var adres = $(href).offset().top;
-  //           $('html, body').animate({
-  //              scrollTop: adres
-  //             }, 1000);
-  //       })
-  //   }
-   //
-  //  zadanie2();
+    function smartScroll(){
 
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 77
-        }, 1000);
-        return false;
-      }
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top - 77
+            }, 1000);
+            return false;
+          }
+        }
+      })
+
+    };
+
+    smartScroll();
+
+    function hideIcon(){
+      var aboutFourth = $('.aboutFourth');
+      var boxLocalize1 = aboutFourth.find('.boxLocalize1');
+      var boxLocalize2 = aboutFourth.find('.boxLocalize2');
+      var boxLocalize3 = aboutFourth.find('.boxLocalize3');
+      var icon1 = aboutFourth.find('.iconFirst');
+      var icon2 = aboutFourth.find('.iconSecond');
+      var icon3 = aboutFourth.find('.iconThird');
+
+      var width = $(window).width();
+        if (width > 1023) {
+
+          boxLocalize1.on('mouseenter', function(){
+            icon1.slideUp(400);
+          });
+
+          boxLocalize1.on('mouseleave', function(){
+            icon1.slideDown(400);
+          });
+
+          boxLocalize2.on('mouseenter', function(){
+            icon2.slideUp(400);
+          });
+
+          boxLocalize2.on('mouseleave', function(){
+            icon2.slideDown(400);
+          });
+
+          boxLocalize3.on('mouseenter', function(){
+            icon3.slideUp(400);
+          });
+
+          boxLocalize3.on('mouseleave', function(){
+            icon3.slideDown(400);
+          });
+        }
     }
-  });
 
+    hideIcon();
+
+    $(window).on('resize', function(){
+       hideIcon();
+   });
+
+   $('.slider').slick();
 
 });
